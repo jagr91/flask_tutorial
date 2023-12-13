1dd217cba3a2e97afd0796868e2021394308da2b
Grzegorz Jastrzebski <grzesiek1991@gmail.com>
2023-12-13 17:30:19 +0100

    Forms

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import random
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mySecretKey"
+
+
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
@@ -17,12 +26,25 @@ def index():
 def user(name):
     return render_template('user.html', user_name=name)
 
+
 # invalid URL - custom page
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 # internal server eror
 @app.errorhandler(500)
-def page_not_found(e):
+def server_error(e):
     return render_template('500.html'), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    
+    return render_template('name.html', name=name, form=form)
